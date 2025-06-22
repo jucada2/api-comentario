2fea43be5ceeb96c00fe57a885c43ad2200e048c
jucada2 <juan.paredes@utec.edu.pe>
2025-06-21 23:31:50 -0500

    upd

--- comentario.py
+++ comentario.py
@@ -1,38 +1,44 @@
 import boto3
-import json
 import uuid
 import os
-from datetime import datetime
+import json
 
 def lambda_handler(event, context):
-    # Obtener el bucket de entorno
-    bucket_name = os.environ.get('INGEST_BUCKET')
 
-    # Parsear el body
-    raw_body = event.get('body', {})
-    if isinstance(raw_body, str):
-        try:
-            comentario = json.loads(raw_body)
-        except json.JSONDecodeError:
-            return {
-                'statusCode': 400,
-                'body': json.dumps({'error': 'JSON inválido'})
-            }
-    else:
-        comentario = raw_body
+    tenant_id = event['body']['tenant_id']
+    texto = event['body']['texto']
+    nombre_tabla = os.environ["TABLE_NAME"]
+    bucket_name = os.environ["INGEST_BUCKET"]
 
-    # Generar nombre único para el archivo
-    timestamp = datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
-    file_name = f"comentario_{timestamp}_{uuid.uuid4().hex}.json"
 
-    # Subir comentario como archivo JSON al bucket
-    s3 = boto3.resource('s3')
-    s3.Object(bucket_name, f"comentarios/{file_name}").put(
+    uuidv1 = str(uuid.uuid1())
+    comentario = {
+        'tenant_id': tenant_id,
+        'uuid': uuidv1,
+        'detalle': {
+            'texto': texto
+        }
+    }
+
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(nombre_tabla)
+    response = table.put_item(Item=comentario)
+
+
+    s3 = boto3.client('s3')
+    s3_key = f"{tenant_id}/{uuidv1}.json"
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=s3_key,
         Body=json.dumps(comentario),
         ContentType='application/json'
     )
 
+
     return {
         'statusCode': 200,
-        'body': {'message': 'Comentario ingresado correctamente', 'archivo': file_name}
+        'comentario': comentario,
+        's3_key': s3_key,
+        'response': response
     }
